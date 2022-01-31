0dfdf33365ca7672c416023f8486890330fc0718
unknown <prandrews@hotmail.co.uk>
2022-01-31 09:08:18 +0000

    Start of video.

---
+++ 03_02/linear_search.py
@@ -0,0 +1,11 @@
+def linear_search(data, target):
+    pass
+
+
+data = [4, 5, 2, 7, 1, 8]
+target = 1
+result = linear_search(data, target)
+if result == -1:
+    print("Item not found.")
+else:
+    print(f"Item found at position {result}.")

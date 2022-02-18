52c812b503ad0b87d9819ff128cf83194e300b70
unknown <prandrews@hotmail.co.uk>
2022-02-18 08:59:43 +0000

    Video retake.

---
+++ 01_01/100_doors_prep.py
@@ -0,0 +1,19 @@
+# doors = [False] * 101
+#
+# print(doors)
+#
+# for i in range(1, 101):
+#     doors[i] = not doors[i]
+#
+# print(doors)
+#
+# for i in range(1, 6):
+#     for j in range(1, 4):
+#         print("i:", i, "j:", j)
+
+for i in range(1, 11, 2):
+    print(i)
+
+
+
+
